17db1eda4d02d82943adeed6be75d43dd0ad0b47
Pewho <mathias.bazire@sylpheo.com>
2013-10-10 11:10:40 +0200

    add logic to generate Cloud

--- app/js/app/controllers/play.js
+++ app/js/app/controllers/play.js
@@ -3,9 +3,29 @@
 /* Controllers */
 
 angular.module('PoneyLand.controllers').
-        controller('playCtrl',["$scope", function(scope) {
+        controller('playCtrl',["$scope","$timeout", function(scope, timeout) {
         $("#rainbowtop").slideUp("slow",function() {
             $('body').css("background-image","url('img/back.png')");
         });
+        scope.cloud = 0;
+        scope.cloudPerSecond = 1;
+        //Cloud Générator
+        scope._generateCloud = function(){
+            scope.cloud+=scope.cloudPerSecond;
+            console.log(scope.cloud);
+            timeout(function(){
+                scope._generateCloud();
+            }, 1000, true);
+        };
+        //Initialisation of it
+        scope._generateCloud();
+        
+        //Action for modfication cloud Rate (-/+ capable)
+        scope.modulRateCloudPerSecond = function(modulation){
+            scope.cloudPerSecond+=parseInt(modulation);
+        }
+        
+        //Colors declarations
+        
         
 }]);
