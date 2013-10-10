6cf0c58297edb3f377d7a22d5dad0884cf5c84ee
miragecentury <victorien.vanroye@gmail.com>
2013-10-10 11:24:22 +0200

    - XD

--- app/js/app/controllers/play.js
+++ app/js/app/controllers/play.js
@@ -3,9 +3,15 @@
 /* Controllers */
 
 angular.module('PoneyLand.controllers').
-        controller('playCtrl',["$scope", function(scope) {
-        $("#rainbowtop").slideUp("slow",function() {
-            $('body').css("background-image","url('img/back.png')");
+        controller('playCtrl', ["$scope", function(scope) {
+        $("#rainbowtop").slideUp("slow", function() {
+            $('body').css("background-image", "url('img/back.png')");
         });
-        
-}]);
+        $(".rainbowcutiemark").animate({
+        }, {
+            step: function(now,fx){
+                
+            }
+        });
+
+    }]);
